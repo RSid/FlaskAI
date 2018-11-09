469ead1ab41c66f136230136d3a9369c5be8b053
Alla Hoffman <alla.hoffman@buildium.com>
2018-11-09 11:54:22 -0500

    rendering template with best practice

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template
+from jinja2 import Environment, PackageLoader, select_autoescape
 
 app = Flask(__name__)
 
+env = Environment(
+    loader=PackageLoader('app', 'templates'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
 @app.route('/')
 def index():
-    return render_template('index.html', username='Alla')
+    template = env.get_template('index.html')
+    return template.render(username='Alla')
